342364049ac15669d81f6ba3ead145d2b2d37313
alu10701951 <alugar1951@ieselcaminas.org>
2023-10-24 20:44:00 +0200

    okpo

--- app/src/main/java/com/example/elsol/Principal.kt
+++ app/src/main/java/com/example/elsol/Principal.kt
@@ -4,37 +4,33 @@ import android.annotation.SuppressLint
 import android.content.res.Configuration
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.Share
-import androidx.compose.material.icons.outlined.Star
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -42,16 +38,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.elsol.ui.theme.ElSolTheme
+import com.example.elsol.ui.theme.Pink40
 import com.example.elsol.ui.theme.Purple40
 import com.example.elsol.ui.theme.PurpleGrey80
 import java.security.Principal
@@ -67,92 +66,68 @@ fun Principal(navController: NavHostController) {
         }
 
         else -> {
-            Scaffold(bottomBar = {
-                TopAppBar(
-                    title = {
-                        Text(text = "CoffeeShops", color = Color.White, fontSize = 20.sp)
-                    },
-                    navigationIcon = {
-                        IconButton(
-                            onClick = {
-                            }
+            Scaffold(
+                //snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+                bottomBar = {
+                    BottomAppBar(
+                        containerColor = Color.Red,
+                        contentColor = Color.White,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            //.zIndex(1f)
+                    )
+                    {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(end = 5.dp)
                         ) {
-                            Icon(
-                                imageVector = Icons.Default.Menu,
-                                contentDescription = null,
-                                tint = Color.White
-                            )
-                        }
-                    },
-                    actions = {
-                        Row() {
-                            IconButton(
-                                onClick = {
-                                    isMenuVisible = true
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                IconButton(
+                                    onClick = {
+                                        //showDrawer = !showDrawer
+                                    }
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.Default.ArrowBack,
+                                        contentDescription = null,
+                                        tint = Color.White
+                                    )
                                 }
-                            ) {
-                                Icon(
-                                    imageVector = Icons.Default.MoreVert,
-                                    contentDescription = null,
-                                    tint = Color.White
-                                )
+                                /*BadgedBox(badge = {
+                                    Text(text = badgeCount.toString(), modifier = Modifier
+                                        .background(Color.Blue, shape = CircleShape)) }) {
+
+                                    Icon(
+
+                                        imageVector = Icons.Default.Favorite,
+                                        contentDescription = null,
+                                        modifier = Modifier.clickable { badgeCount++ },
+                                        tint = Color.White
+                                    )
+                                }*/
                             }
-                        }
-                        Row() {
-                            DropdownMenu(
-                                expanded = isMenuVisible,
-                                onDismissRequest = {
-                                    isMenuVisible = false
-                                },
-                                modifier = Modifier.background(PurpleGrey80)
-                            ) {
-                                DropdownMenuItem(
-                                    text = {
-                                        Text(
-                                            text = "Compartir",
-                                            color = Color.Black,
-                                            fontSize = 16.sp
-                                        )
-                                    },
-                                    onClick = { isMenuVisible = false },
-                                    leadingIcon = {
-                                        Icon(
-                                            imageVector = Icons.Default.Share,
-                                            contentDescription = null,
-                                            tint = Color.Black
-                                        )
-                                    },
-                                )
-                                DropdownMenuItem(
-                                    text = {
-                                        Text(
-                                            text = "Album",
-                                            color = Color.Black,
-                                            fontSize = 16.sp
-                                        )
-                                    },
-                                    onClick = { isMenuVisible = false },
-                                    leadingIcon = {
-                                        Icon(
-                                            imageVector = Icons.Default.Lock,
-                                            contentDescription = null,
-                                            tint = Color.Black
-                                        )
-                                    },
-                                )
+                            Row {
+                                FloatingActionButton(onClick = { /*TODO*/ }, containerColor = Pink40) {
+                                    Icon(
+                                        imageVector = Icons.Default.Add,
+                                        contentDescription = null,
+                                        tint = Color.Black
+                                    )
+                                }
                             }
-                        }
-                    },
-                    colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = Purple40)
-                )
-            }
-            ) {
-                Column(modifier = Modifier.padding(top = 60.dp)) {
-                    LazyColumn {
-                        items(getCardCoffee()) { lazy ->
-                            ItemsCoffe(cardCoffee = lazy, navController = navController)
                         }
                     }
+                }
+            )
+            {
+                Column(modifier = Modifier.padding(bottom = 60.dp)) {
+                    LazyVerticalGrid(columns = GridCells.Fixed(2),
+                        content = {items(getCardCoffee()) { lazy ->
+                        ItemsCoffe(cardCoffee = lazy, navController = navController)}
+                    } )
                 }
             }
 
@@ -203,7 +178,7 @@ fun ItemsCoffe(cardCoffee: CardCoffee, navController: NavHostController) {
         Modifier
             .fillMaxWidth()
             .padding(16.dp)
-            .clickable { navController.navigate("Comentarios/${cardCoffee.name}") },
+            .clickable { },
         elevation = CardDefaults.cardElevation(10.dp)
     ) {
         Column(
@@ -214,89 +189,73 @@ fun ItemsCoffe(cardCoffee: CardCoffee, navController: NavHostController) {
                 contentDescription = "Coffee",
                 modifier = Modifier
                     .fillMaxWidth()
-                    .size(200.dp),
+                    .size(175.dp),
                 contentScale = ContentScale.Crop
             )
-        }
-        Scaffold(bottomBar = {
-            TopAppBar(
-                title = {
-                    Text(text =cardCoffee.name, color = Color.White, fontSize = 20.sp)
-                },
-                navigationIcon = {
+            BottomAppBar(modifier = Modifier.height(55.dp)) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = cardCoffee.name, modifier = Modifier.padding(start = 5.dp))
                     IconButton(
                         onClick = {
+                            isMenuVisible = true
                         }
                     ) {
                         Icon(
-                            imageVector = Icons.Default.Menu,
-                            contentDescription = null,
-                            tint = Color.White
+                            imageVector = Icons.Default.MoreVert,
+                            contentDescription = "Abrir menú"
                         )
                     }
-                },
-                actions = {
-                    Row() {
-                        IconButton(
-                            onClick = {
-                                isMenuVisible = true
-                            }
-                        ) {
-                            Icon(
-                                imageVector = Icons.Default.MoreVert,
-                                contentDescription = null,
-                                tint = Color.White
-                            )
-                        }
-                    }
-                    Row() {
-                        DropdownMenu(
-                            expanded = isMenuVisible,
-                            onDismissRequest = {
-                                isMenuVisible = false
-                            },
-                            modifier = Modifier.background(PurpleGrey80)
-                        ) {
-                            DropdownMenuItem(
-                                text = {
-                                    Text(
-                                        text = "Compartir",
-                                        color = Color.Black,
-                                        fontSize = 16.sp
-                                    )
-                                },
-                                onClick = { isMenuVisible = false },
-                                leadingIcon = {
-                                    Icon(
-                                        imageVector = Icons.Default.Share,
-                                        contentDescription = null,
-                                        tint = Color.Black
-                                    )
-                                },
-                            )
-                            DropdownMenuItem(
-                                text = {
-                                    Text(
-                                        text = "Album",
-                                        color = Color.Black,
-                                        fontSize = 16.sp
-                                    )
-                                },
-                                onClick = { isMenuVisible = false },
-                                leadingIcon = {
-                                    Icon(
-                                        imageVector = Icons.Default.Lock,
-                                        contentDescription = null,
-                                        tint = Color.Black
-                                    )
-                                },
-                            )
-                        }
-                    }
-                },
-            )
+                }
 
-        }){ Text(text = "hola")}
+                DropdownMenu(
+                    expanded = isMenuVisible,
+                    onDismissRequest = { isMenuVisible = false },
+                    offset = DpOffset(0.dp, ((-40).dp))
+                )
+                {
+                    DropdownMenuItem(
+                        text = {
+                            Text(
+                                text = "Copiar",
+                                color = Color.Black,
+                                fontSize = 16.sp
+                            )
+                        },
+                        onClick = { isMenuVisible = false },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.Add,
+                                contentDescription = null,
+                                tint = Color.Black
+                            )
+                        },
+                    )
+                    DropdownMenuItem(
+                        text = {
+                            Text(
+                                text = "Eliminar",
+                                color = Color.Black,
+                                fontSize = 16.sp
+                            )
+                        },
+                        onClick = { isMenuVisible = false },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.Delete,
+                                contentDescription = null,
+                                tint = Color.Black
+                            )
+                        },
+                    )
+                }
+            }
+
+
+        }
     }
 }
 
